7fd7c9105734e998d5283d3a8dda063a99c733b1
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2025-01-28 13:02:55 -0400

    init actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
-
+import { useState } from 'react'
+const key = "saludandoAmoises"
 export default function App() {
+  const [isValid, setIsValid] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [value, setValue] = useState("")
+
+  function validateKey() {
+    setLoading(true)
+    setTimeout(() =>{
+      try {
+        if (key !== value) {
+          throw { message: "Key incorrecta" }
+        }
+        setIsValid(true)
+  
+      } catch (error) {
+        setError(true)
+        setValue("")
+      } finally {
+        setLoading(false)
+      }
+    }, 3000)
+  }
+
+  function onChange(e) {
+    setError(false)
+    setValue(e.target.value)
+  }
+
   return (
-      <Validate/>
+    <>
+      {isValid ?
+        <Welcome
+          loading={loading}
+        />
+        :
+        <Validate
+          loading={loading}
+          error={error}
+          value={value}
+          onChange={onChange}
+          onValdation={validateKey}
+        />
+      }
+    </>
+
   )
 }
 
 
-export function Validate() {
+export function Validate({ loading, error, onChange, value, onValdation }) {
+
   return (
     <div className='w-full h-screen bg-slate-100 flex items-center justify-center'>
 
@@ -18,14 +63,22 @@ export function Validate() {
           type="text"
           placeholder='Type your key'
           className='w-4/5 mx-auto flex mt-4 h-9 border rounded-md px-2 focus:bg-slate-100'
+          value={value}
+          onChange={onChange}
         />
-        {/* <p className='w-4/5 block mx-auto my-2'>Loading...</p>
-        <p className='w-4/5 block mx-auto my-2 text-red-600'>Error</p> */}
+
+        {loading &&
+          <p className='w-4/5 block mx-auto my-2'>Loading...</p>
+        }
+        {error &&
+          <p className='w-4/5 block mx-auto my-2 text-red-600'>Error: Key incorrecta</p>
+        }
+
 
         <div className='flex w-4/5 mx-auto justify-end gap-8 pt-8'>
           <button className='h-9 bg-red-600 px-3 rounded-md text-white font-semibold hover:bg-red-700 cursor-pointer'>Cancelar</button>
 
-          <button className='h-9 bg-blue-600 px-3 rounded-md text-white font-semibold hover:bg-blue-700 cursor-pointer'>Aceptar</button>
+          <button className='h-9 bg-blue-600 px-3 rounded-md text-white font-semibold hover:bg-blue-700 cursor-pointer' onClick={onValdation}>Aceptar</button>
         </div>
 
       </div>
@@ -34,14 +87,16 @@ export function Validate() {
   )
 }
 
-export function Welcome() {
+export function Welcome({ loading }) {
   return (
     <div className='w-full h-screen bg-slate-100 flex items-center justify-center'>
 
       <div className='w-4/5  bg-white shadow-md py-8 rounded-md'>
         <h1 className='text-center text-2xl font-bold'>Welcom usuario</h1>
 
-        {/* <p className='w-4/5 block mx-auto my-2'>Loading...</p> */}
+        {loading &&
+          <p className='w-4/5 block mx-auto my-2'>Loading...</p>
+        }
 
         <div className='flex w-4/5 mx-auto justify-end gap-8 pt-8'>
           <button className='h-9 bg-blue-600 px-3 rounded-md text-white font-semibold hover:bg-blue-700 cursor-pointer'>Cerrar session</button>
